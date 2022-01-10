ce48a6bde4f4dda775ce16749b5d79c22ee20092
gladys5 <yumanji@live.com>
2022-01-10 15:32:53 -0700

    apirick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+//import React,{useState}from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import Pagination from "./components/Pagination/Pagination"
+import Search from "./components/Search/Search";
+import ReactPaginate from "react-paginate";
 
 function App() {
+  let [axiosData,setAxiosData]=useState([])
+  let {info,results}= axiosData
+  let [pageNumber,setPageNumber]=useState(1)
+  let [search,setSearch]= useState("")
+
+   console.log(pageNumber)
+
+
+
+
+
+  
+useEffect(()=>{
+  axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`)
+  .then(res=>{setAxiosData(res.data)
+    
+   
+  })
+  
+
+
+}, [pageNumber, search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <h1 className="text-center  my-4">
+       Rick & Morty Aplicasion </h1>
+        <Search setPageNumber={setPageNumber} setSearch={setSearch}/>
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+                  <Filters/>
+              </div> 
+                 <div className="col-8">
+                   <div className="row">
+                      <Cards results={results}/>
+                   </div>
+                 </div>
+             </div>
+         </div>
+         <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+
+
+const Cards = ({ results }) => {
+    let display;
+     //console.log(results)
+    if (results) {
+         display = results.map(( X) => {
+               let {id, name, image, location, status } = X;
+               return ( 
+            
+               <div key={id} className="col-4 position-relative">
+                 
+                   <div className="borde">
+                     <img src={image} alt="" className="img-fluid" />
+                      <div className="content">
+                        <div className="fs-4 fw-bold mb-4">{name}</div>
+                        <div className="">
+                          <div className="fs-6">Last location</div>
+                           <div className="fs-5">{location.name}</div>
+                        </div>
+                      </div>
+                   </div> 
+
+                  {(()=>{
+                    if(status==="dead"){
+                      return(
+                        <div className= "position-absolute bg-danger">{status}</div>
+                      )
+                    }
+                    else if(status==="Alive"){
+                      return(
+                        <div className= "position-absolute bg-success">{status}</div>
+                      )
+                    } 
+                    else{
+                      return(
+                        <div className= "position-absolute bg-secondary">{status}</div>
+                      )
+                    }
+            
+                  })()}
+                  
+               </div>
+            );
+        });
+    } else {
+      display=  "No Characters Found:/";
+    }
+
+    return <> {display}</>
+    
+};
+
+export default Cards;
